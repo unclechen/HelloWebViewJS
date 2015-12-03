939233bbacd5506c1ae42af1ca7cb1b1b41791c7
unclechen <xh-chen@163.com>
2015-12-03 18:25:33 +0800

    fix

--- app/src/main/java/com/nought/hellowebviewjs/MainActivity.java
+++ app/src/main/java/com/nought/hellowebviewjs/MainActivity.java
@@ -1,7 +1,7 @@
 package com.nought.hellowebviewjs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
